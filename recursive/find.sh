19f41da011efbf921530615d6534df449d69e1c5
Jacob Nollette <jacob@jacobnollette.com>
2017-09-15 16:14:49 -0500

    cleaned things up

--- recursive/find.sh
+++ recursive/find.sh
@@ -1,35 +1,29 @@
 #!/bin/bash
 
-
-
-
-
 processVideo() {
-	
-	
+
 	filenameExtension="mkv";
 	preset="medium";
 	threads=0;
 	width=720;
 	bitrate="225k";
-	
-	
+  soundBitrate="256k"
+
 	for item in "$@"; do
 		original_item=$item;
 		item="$( echo "$item" | sed 's/ /\\ /g' )";
-		
-		
+
 		#	create proper file name, quotes will be needed
 		season_folder=`dirname "$item"`;
 		season_folder="$( echo "$season_folder" | sed "s@\\\\@@g" )";
-		
+
 		#	this is the series folder, parent to season
 		season_root=`dirname "$season_folder"`;
-		
-		#	season folder
+
+		#	season folder - season name or number
 		season_parent_folder_name=`basename "$season_folder"`;
-		
-		
+
+
 		# create print folder;
 		cd "$season_root";
 		mkdir -p "print";
@@ -37,46 +31,22 @@ processVideo() {
 		mkdir -p "$season_parent_folder_name";
 		cd "$season_parent_folder_name";
 		print_dir=`pwd`;							#	here's our print directory
-		
-		
-		
-		
-		
-		filename=`basename "$item"`;
+
+
+		# get the filename without extension
+    filename=`basename "$item"`;
 		tFilename=${filename%.*};
-		
-		
+
+    #generate print file
 		print_file="$print_dir/$tFilename.";
 		print_file="$print_file$filenameExtension";
-		#echo $print_file;
 
-		
-		
+		ffmpeg -y -i "$original_item" -vf scale="w=$width:trunc(ow/a/2)*2" -c:v libx265 -preset $preset -b:v $bitrate -c:a aac -b:a $soundBitrate -pass 1 -strict -2 -threads $threads -f matroska "$print_file";
 
-		ffmpeg -y -i "$original_item" -vf scale="w=$width:trunc(ow/a/2)*2" -c:v libx265 -preset $preset -b:v $bitrate -c:a aac -b:a 256k -pass 1 -strict -2 -threads $threads -f matroska "$print_file";
-
-		
 	done;
 
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 export -f processVideo;
-find "$@" -type f -exec file -N -i -- {} + | sed -n 's!: video/[^:]*$!!p' | sed 's/ /\\ /g' | xargs bash -c 'processVideo "$@"'	
+find "$@" -type f -exec file -N -i -- {} + | sed -n 's!: video/[^:]*$!!p' | sed 's/ /\\ /g' | xargs bash -c 'processVideo "$@"'
