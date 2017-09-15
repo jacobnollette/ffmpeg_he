87481f8417f3e2a76e2fa5f6e695eed9508a05b2
Jacob Nollette <jacob@jacobnollette.com>
2017-09-15 14:01:25 -0500

    bang

--- folders_with/find.sh
+++ folders_with/find.sh
@@ -1,6 +1,11 @@
 
 #find $@ -type f -exec file -N -i -- {} + | grep video
-find $@ -type f -exec file -N -i -- {} + | sed -n 's!: video/[^:]*$!!p'
 
 
 
+#find "$@" -type f -exec file -N -i -- {} + | sed -n 's!: video/[^:]*$!!p'
+find "$@" -type f -exec file -N -i -- {} + | sed -n 's!: video/[^:]*$!!p'
+#find "$@" -type f -exec file -N -i -- {} + | grep video
+
+
+# | xargs -I {} processVideo -c "{}";
