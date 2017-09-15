e1139307986ea4e99af5d5305aaab6abc83c8b19
Jacob Nollette <jacob@jacobnollette.com>
2017-09-15 16:07:39 -0500

    find command

--- folders_with/find.sh
+++ folders_with/find.sh
@@ -5,9 +5,57 @@
 
 
 processVideo() {
-	echo "$@";
+	
+	
+	filenameExtension="mkv";
+	preset="medium";
+	threads=0;
+	width=720;
+	bitrate="225k";
+	
+	
 	for item in "$@"; do
-		echo "bang $item";
+		original_item=$item;
+		item="$( echo "$item" | sed 's/ /\\ /g' )";
+		
+		
+		#	create proper file name, quotes will be needed
+		season_folder=`dirname "$item"`;
+		season_folder="$( echo "$season_folder" | sed "s@\\\\@@g" )";
+		
+		#	this is the series folder, parent to season
+		season_root=`dirname "$season_folder"`;
+		
+		#	season folder
+		season_parent_folder_name=`basename "$season_folder"`;
+		
+		
+		# create print folder;
+		cd "$season_root";
+		mkdir -p "print";
+		cd print;
+		mkdir -p "$season_parent_folder_name";
+		cd "$season_parent_folder_name";
+		print_dir=`pwd`;							#	here's our print directory
+		
+		
+		
+		
+		
+		filename=`basename "$item"`;
+		tFilename=${filename%.*};
+		
+		
+		print_file="$print_dir/$tFilename.";
+		print_file="$print_file$filenameExtension";
+		#echo $print_file;
+
+		
+		
+
+		ffmpeg -y -i "$original_item" -vf scale="w=$width:trunc(ow/a/2)*2" -c:v libx265 -preset $preset -b:v $bitrate -c:a aac -b:a 256k -pass 1 -strict -2 -threads $threads -f matroska "$print_file";
+
+		
 	done;
 
 }
@@ -31,4 +79,4 @@ processVideo() {
 
 
 export -f processVideo;
-find "$@" -type f -exec file -N -i -- {} + | sed -n 's!: video/[^:]*$!!p' | sed 's/ /_/g' | xargs bash -c 'processVideo "$@"'	
+find "$@" -type f -exec file -N -i -- {} + | sed -n 's!: video/[^:]*$!!p' | sed 's/ /\\ /g' | xargs bash -c 'processVideo "$@"'	
