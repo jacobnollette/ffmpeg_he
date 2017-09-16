f024c85e5bab4dcca58be49046d56ddd81f9ced1
Jacob Nollette <jacob@jacobnollette.com>
2017-09-15 22:50:33 -0500

    somethings don't work in linux

--- killEncodeByName.sh
+++ killEncodeByName.sh
@@ -1,4 +1,4 @@
 #	this is how you kill rogue ffmpeg processes
 # no quote input searched your processes and kills them
 
-ps -ef | grep "$@" | cut -d " " -f 2 #| xargs kill;
+#ps -ef | grep "$@" | sed 's/\s\s*/ /g' | sort -n #| cut -d " " -f 2 | xargs kill;
