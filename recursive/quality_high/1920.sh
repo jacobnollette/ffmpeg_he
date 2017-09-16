c9884f9f08c842ced18bbb3ef330f2b51218814a
Jacob Nollette <jacob@jacobnollette.com>
2017-09-16 15:57:50 -0500

    quality high

---
+++ recursive/quality_high/1920.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+filenameExtension="mkv";
+filetype="matroska";
+videocodec="libx265";         # always h265
+audiocodec="aac";             # aac is alright
+videobitrate="1700k";
+audiobitrate="256k"
+preset="medium";
+threads=0;                    #unlimited threads
+width=1920;
+
+
+
+
+source ../core/superrecursive.sh;
+
+export -f processVideo;
+
+
+
+#	here is out input stack
+export filenameExtension;
+export filetype;
+export videocodec;
+export audiocodec;
+export videobitrate;
+export audiobitrate;
+export preset;
+export threads;
+export width;
+
+
+#	pass out root input as a variables
+sourceInput="$@";
+export sourceInput;
+
+find "$sourceInput" -type f | grep -E "\.webm$|\.flv$|\.vob$|\.ogg$|\.ogv$|\.drc$|\.gifv$|\.mng$|\.avi$|\.mov$|\.qt$|\.wmv$|\.yuv$|\.rm$|\.rmvb$|/.asf$|\.amv$|\.mp4$|\.m4v$|\.mp4$|\.m?v$|\.svi$|\.3gp$|\.flv$|\.f4v$|\.mkv$" | cut -d ':' -f 1 | sed 's/.*/"&"/' | xargs bash -c 'processVideo "$sourceInput" "$filenameExtension" "$filetype" "$videocodec" "$audiocodec" "$videobitrate" "$audiobitrate" "$preset" "$threads" "$width" "$@"';
