beb0295de617574ee4de657e017d00cbc6b26b3f
Jacob Nollette <jacob@jacobnollette.com>
2017-09-16 01:19:08 -0500

    super recursive

--- super_recursive/0480.sh
+++ super_recursive/0480.sh
@@ -18,10 +18,17 @@ function processVideo480() {
 	width=480;
 	videobitrate="115k";
 	soundBitrate="256k"
-	
 
+	#	here is our input folder;
+	rootItem="$1";
+	rootItemUgly="$( echo "$rootItem" | sed 's/ /\\ /g' )";
+
+	#echo $rootItem;
 	theItems="${@:2}";
-	for item in "$theItems"; do
+	for item in "${@:2}"; do
+
+		#	here is the found file
+		echo "$item";
 		original_item=$item;
 		# add escape characters
 		item="$( echo "$item" | sed 's/ /\\ /g' )";
@@ -30,35 +37,51 @@ function processVideo480() {
 		season_folder=`dirname "$item"`;
 		#remove escape characters,- honestly I don't know why we had to do this
 		season_folder="$( echo "$season_folder" | sed "s@\\\\@@g" )";
+		#echo $original_item;
 
 		#	this is the series folder, parent to season
-		season_root=`dirname "$season_folder"`;
+		season_root=`dirname "$original_item"`;
+		#echo $season_root;
 
 		#	season folder - season name or number
 		season_parent_folder_name=`basename "$season_folder"`;
+		root_parent_folder=`basename "$rootItem"`;
+
+		if [[ "$season_parent_folder_name" == "$root_parent_folder" ]]; then
+			print_folder="$rootItem/print";
+		else
+
+			print_folder="$rootItem/print/$season_parent_folder_name";
+		fi
+
+		#	fucking os x
+		doubleBase=`basename "$print_folder"`;
+		if [[ "$doubleBase" == ".AppleDouble" ]]; then
+			print_folder=`dirname $print_folder`;
+		fi
+
+		#	create print folder
+		mkdir -p "$print_folder";
+		#echo $print_folder;
+		#echo $season_parent_folder_name;
+		#echo $season_parent_folder_name;
 
 
-		# create print folder;
-		cd "$season_root";
-		mkdir -p "print";
-		cd print;
-		mkdir -p "$season_parent_folder_name";
-		cd "$season_parent_folder_name";
-		print_dir=`pwd`;							#	here's our print directory
+		#echo "$print_folder";
 
 
 		# get the filename without extension
-		filename=`basename "$item"`;
+		filename=`basename "$original_item"`;
 		fn_no_extension=${filename%.*};
 
 		#generate print file
-		print_file="$print_dir/$fn_no_extension.";
+		print_file="$print_folder/$fn_no_extension";
 		print_file="$print_file$filenameExtension";
 
 		# strict 2 for aac, because it's experimental
-		#ffmpeg -y -i "$original_item" -vf scale="w=$width:trunc(ow/a/2)*2" \
-		#-c:v $videocodec -preset $preset -b:v $videobitrate -c:a $audiocodec \
-		#-b:a $soundBitrate -pass 1 -strict -2 -threads $threads -f $filetype "$print_file";
+		ffmpeg -y -i "$original_item" -vf scale="w=$width:trunc(ow/a/2)*2" \
+		-c:v $videocodec -preset $preset -b:v $videobitrate -c:a $audiocodec \
+		-b:a $soundBitrate -pass 1 -strict -2 -threads $threads -f $filetype "$print_file";
 
 	done;
 
