7be20e5e73775230e2cfd3608163f83129693802
Jacob Nollette <jacob@jacobnollette.com>
2017-10-01 23:10:20 +0000

    new scripts

--- install/ubuntu/install_step_1.sh
+++ install/ubuntu/install_step_1.sh
@@ -16,30 +16,5 @@ PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/f
 make
 make install
 
-mkdir ~/ffmpeg_sources;
-cd ~/ffmpeg_sources
-wget http://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
-tar xjvf ffmpeg-snapshot.tar.bz2
-cd ffmpeg
-PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure \
---prefix="$HOME/ffmpeg_build" \
---pkg-config-flags="--static" \
---extra-cflags="-I$HOME/ffmpeg_build/include" \
---extra-ldflags="-L$HOME/ffmpeg_build/lib" \
---bindir="$HOME/bin" \
---enable-gpl \
---enable-libass \
---enable-libfdk-aac \
---enable-libfreetype \
---enable-libmp3lame \
---enable-libopus \
---enable-libtheora \
---enable-libvorbis \
---enable-libvpx \
---enable-libx264 \
---enable-libx265 \
---enable-nonfree
-PATH="$HOME/bin:$PATH" make
-make install
-make distclean
-hash -r:
+
+apt-get install -y x264 x265;
