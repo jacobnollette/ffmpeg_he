339b53532b54e6d727d35408a4477b2829b817a0
Jacob Nollette <jacob@jacobnollette.com>
2017-10-03 21:38:23 -0500

    reworked install process, should work now

--- install/ubuntu/install_step_1.sh
+++ install/ubuntu/install_step_1.sh
@@ -6,15 +6,5 @@ sudo apt-get install -y yasm
 sudo apt-get install -y libx264-dev libx265-dev libmp3lame-dev libopus-dev libvpx-dev
 
 sudo add-apt-repository ppa:jonathonf/ffmpeg-3
-apt-get install -y x264 x265;
+apt-get install -y x264;
 
-sudo apt-get install -y cmake mercurial
-cd ~/ffmpeg_sources
-hg clone https://bitbucket.org/multicoreware/x265
-cd ~/ffmpeg_sources/x265/build/linux
-PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED:bool=off ../../source
-make
-make install
-
-
-apt-get install -y x264 x265;
---
+++ install/ubuntu/install_step_3.sh
@@ -0,0 +1,28 @@
+
+mkdir ~/ffmpeg_sources;
+cd ~/ffmpeg_sources
+wget http://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
+tar xjvf ffmpeg-snapshot.tar.bz2
+cd ffmpeg
+PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure \
+--prefix="$HOME/ffmpeg_build" \
+--pkg-config-flags="--static" \
+--extra-cflags="-I$HOME/ffmpeg_build/include" \
+--extra-ldflags="-L$HOME/ffmpeg_build/lib" \
+--bindir="$HOME/bin" \
+--enable-gpl \
+--enable-libass \
+--enable-libfdk-aac \
+--enable-libfreetype \
+--enable-libmp3lame \
+--enable-libopus \
+--enable-libtheora \
+--enable-libvorbis \
+--enable-libvpx \
+--enable-libx264 \
+--enable-libx265 \
+--enable-nonfree
+PATH="$HOME/bin:$PATH" make
+make install
+make distclean
+hash -r:
